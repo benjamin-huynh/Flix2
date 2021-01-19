887d07c9e7fea0a6595d3eff8c4f50cd5fbe78c3
Benjamin Huynh <ben.huynh408@gmail.com>
2021-01-18 20:36:06 -0800

    first commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ben Huynh on 1/29/20.
+//  Copyright © 2020 Ben Huynh. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+}
